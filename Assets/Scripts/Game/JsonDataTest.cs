16e122461a40d345a6edda5f1bb68bd88c40b6a8
関口晃平 <mccc.kohei@icloud.com>
2022-02-05 13:06:27 +0900

    データの初期値を変更

--- Assets/Scripts/Game/JsonDataTest.cs
+++ Assets/Scripts/Game/JsonDataTest.cs
@@ -7,14 +7,51 @@ public class JsonDataTest : MonoBehaviour
     string file = "Test.json";
     string file2 = "data.json";
 
+    RankingManager _RankingManager = null;
+    int _PreviousDate = 1;
+    public int day = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start JsonDataTest");
-        var data_instance = GameData.Instance.CurrentGameData;
+        //GameData.ResetData();
+        //var data_instance = GameData.Instance.CurrentGameData;
 
-        GameDataManager.SaveData(data_instance, file);
-        GameDataManager.LoadData<GameData>(file);
+        //GameData.Instance.CurrentGameData = GameDataManager.LoadData<GameData.Data>(file);
+        //GameDataManager.SaveData(data_instance, file);
+
+        //GameDataManager.LoadData<GameData>(file);
+        _RankingManager = FindObjectOfType<RankingManager>().GetComponent<RankingManager>();
+
+        /*
+        var array = GameData.Instance.IntArray;
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i*2;
+            Debug.Log("[" + i + "]:" + GameData.Instance.IntArray[i]);
+        }
+        Debug.Log("-----------------------------");
+        for (int i = 0; i < array.Length; i++)
+        {
+            GameData.Instance.IntArray[i] = i+5;
+            Debug.Log("[" + i + "]:" + array[i]);
+        }
+        Debug.Log("---------staic------------");
+        for(int i = 0;i < s_Int_Array.Length;i++)
+        {
+            Debug.Log(s_Int_Array[i]);
+        }
+        */
+    }
+
+    void Update()
+    {
+        if (day != _PreviousDate)
+        {
+            _PreviousDate = day;
+            _RankingManager.UpdateRankingView(day);
+        }
 
     }
 }
