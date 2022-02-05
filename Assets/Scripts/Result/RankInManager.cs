3adfdf62862350240ff9a7abe59f55e84b0ae14e
関口晃平 <mccc.kohei@icloud.com>
2022-02-05 16:51:02 +0900

    ランクインした時の処理をするクラスを作成

---
+++ Assets/Scripts/Result/RankInManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankInManager : MonoBehaviour
+{
+    [SerializeField] private Button[] _Buttons = null;
+    private Animation = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
